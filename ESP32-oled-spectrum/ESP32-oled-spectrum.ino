e54982aefa1f272044e5813c35ae806ad4388ff2
colonelwatch <53552802+colonelwatch@users.noreply.github.com>
2020-10-29 22:16:09 -0700

    General cleanup - Cleaned up comments - Removed unnecessary code and variables - Other minor tweaks

--- ESP32-oled-spectrum/ESP32-oled-spectrum.ino
+++ ESP32-oled-spectrum/ESP32-oled-spectrum.ino
@@ -21,28 +21,27 @@
 #define SAMPLES 2048                  // Must be a power of 2. Raise for higher resolution
                                       //  (less banding) and lower for faster performance.
                                       //  Currently cannot greater than 2048.
-#define SAMPLING_FREQUENCY 44100      // Hz, changing this not recommended
+#define SAMPLING_FREQUENCY 44100      // Hz, changing not recommended
 #define MAX_FREQUENCY 14000           // Hz, must be 1/2 of sampling frequency or less
 #define MIN_FREQUENCY 40              // Hz, cannot be 0, decreasing causes banding
 // Post-processing settings
-#define TIME_FACTOR 4                 // Configures rise smoothing function (raise
-                                      //  for smoother output, lower for dynamic
-                                      //  output)
-#define TIME_FACTOR2 4                // Configures fall smoothing function (raise
-                                      //  for smoother output, lower for dynamic
-                                      //  output.
+#define TIME_FACTOR 4.0               // Configures rise smoothing (factor of exponential
+                                      //  moving average)
+#define TIME_FACTOR2 4.0              // Configures fall smoothing (same as above)
 #define SENSITIVITY 0.125             // FFT output multiplier before post-processing.
-                                      //  Decrease for less amplitude artifacting, increase
-                                      //  for sensitivity
+                                      //  (increasing trades more output artifacting
+                                      //  for sensitivity)
+#define THRESHOLD 32                  // Minimum threshold for log-scale frequency mapping,
+                                      //  necessary to reduce noise artifacts (changing not
+                                      //  recommended)
 #define CAP 100                       // Use to map post-processed FFT output to
-                                      //  display (raise for longer bars, lower
-                                      //  to shorter bars)
+                                      //  display (changing not recommended)
 #define FLOOR 0                       // Use to cut off the buttom of the post-processed
                                       //  output. This can hide noise at the cost of
                                       //  dynamic range.
 // Device settings
-#define COLUMNS 32                    // Number of columns to display, fewer columns
-                                      //  will cause less banding
+#define COLUMNS 32                    // Number of columns to display (fewer columns will
+                                      //  cause less banding)
 #define COLUMN_SIZE 2                 // Size of columns in pixels
 
 #define INPUT_PIN 36 // Labeled VP
@@ -61,8 +60,6 @@ const int sample_period = 1000000/SAMPLING_FREQUENCY;
 int frames;
 volatile int refresh;
 
-volatile float lin_fn[SAMPLES/2], log_fn[COLUMNS];  // Stores frequencies of FFT bins and output bands
-
 template <typename TYPE> class doubleBuffer{
   public:
     volatile TYPE *readBuffer, *writeBuffer;
@@ -77,25 +74,25 @@ template <typename TYPE> class doubleBuffer{
       writeBuffer = (TYPE*)calloc(size, sizeof(TYPE));
     }
 };
-doubleBuffer<uint8_t> screenBuffer(64); // Note to self, alter buffer sizes for OLED
+doubleBuffer<uint8_t> screenBuffer(COLUMNS);
 
 template <typename TYPE> class circularBuffer{
   public:
     int buffer_size;
     volatile TYPE *buffer;
-    volatile int index;
+    volatile int write_index;
     volatile bool available = true;
     void insert(TYPE val){
-      buffer[index] = val;
-      index++;
-      index %= buffer_size;
+      buffer[write_index] = val;
+      write_index++;
+      write_index %= buffer_size;
     }
     circularBuffer(int size){
       buffer_size = size;
       buffer = (TYPE*)calloc(size, sizeof(TYPE));
     }
 };
-circularBuffer<int> analogBuffer(SAMPLES);
+circularBuffer<int16_t> analogBuffer(SAMPLES);
 
 void watchdogReset();
 //*Flash display function prototype removed
@@ -104,10 +101,10 @@ int8_t int_sqrt(int16_t val);
 /* Core 0 Interrupt thread */
 hw_timer_t * timer = NULL;
 void IRAM_ATTR onTimer(){
-  // Basic function is to record new values for circular analogBuffer, but in cases where
+  // Basic function is to insert new values for circular analogBuffer, but in cases where
   // the interrupt is triggered while the buffer is being read from, for the sake of
   // accuracy the values must be stored in a contingency buffer. Then, when the interrupt
-  // is triggered againt and the reading is over, the values are transferred from the
+  // is triggered again and the reading is over, the values are transferred from the
   // contingency buffer to analogBuffer.
   
   static int contigBuffer[SAMPLES];
@@ -127,7 +124,8 @@ void IRAM_ATTR onTimer(){
 /* Core 0 thread */
 TaskHandle_t Task1;
 void Task1code( void * pvParameters ){
-  // Prepares frequency information for scaling algorithm, credit to Rainmeter project (see below)
+  // Preps frequency information for scaling algorithm (credit to Rainmeter project below)
+  float lin_fn[SAMPLES/2], log_fn[COLUMNS];
   for(int i = 0; i < SAMPLES/2; i++) lin_fn[i] = SAMPLING_FREQUENCY * (i + 0.5) / SAMPLES;
   float f_step = (log((float)MAX_FREQUENCY/MIN_FREQUENCY)/log(2.)) / COLUMNS;
   for(int i = 0; i < COLUMNS; i++){
@@ -142,22 +140,16 @@ void Task1code( void * pvParameters ){
   timerAlarmEnable(timer);
   
   while(true){
-    // Debouncing code that checks if the BOOT button has been pressed and, if
-    // so, changes the audio source between the microphone and the 3.5mm jack
-    
     int sum = 0;
     int16_t vReal[SAMPLES] = {0};
     int16_t vImag[SAMPLES] = {0};
-    /*SAMPLING*/
-    // Reads entire analog buffer for FFT calculations. This means a LOT of
-    // redundant data but its necessary to because using new data every time
-    // requires very long delays in total when recording at low frequencies.
-    while(!analogBuffer.available) watchdogReset();
+    // Reads ENTIRE circular analog buffer starting from analogBuffer.write_index, this
+    //  means a lot of overlapping data, but it decouples FFT calculations from sampling
     analogBuffer.available = false;   // Closes off buffer to prevent corruption
     // Reads entire circular buffer, starting from analogBuffer_index
     for(int i = 0; i < SAMPLES; i++){
       // Samples are upscaled to maximize precision at later stages
-      vReal[i] = analogBuffer.buffer[(i+analogBuffer.index)%SAMPLES]*16;
+      vReal[i] = analogBuffer.buffer[(i+analogBuffer.write_index)%SAMPLES]*16;
       sum += vReal[i];
     }
     analogBuffer.available = true;    // Restores access to buffer
@@ -169,66 +161,46 @@ void Task1code( void * pvParameters ){
     // Finds size of samples in a power of 2 and does the FFT
     int power = 1;
     while(SAMPLES != 1 << power) power++;
-    fft_windowing_Hamming(vReal,power);     
-                                    // Hamming windowing function based on a
-                                    //  Hann windowing function I found online
-                                    //  for 8-bit numbers and adapted for the
-                                    //  original 16-bit library. Reduces FFT
-                                    //  amplitudes by around 2 to 4.
-    fix_fft(vReal,vImag,power,0);   // Faster, real version is not used because
-                                    //  of frequency response issues.
-                                    //  TO-DO: Fix the problems and implement?
+    fft_windowing_Hamming(vReal, power);  // Hamming windowing function based on a Hann 
+                                          //  windowing function and adapted for the 
+                                          //  original 16-bit library. Reduces FFT amplitudes
+                                          //  by around 2 to 4.
+    fix_fft(vReal, vImag, power, 0);
     
     for(int i = 0; i < SAMPLES/2; i++)
       vReal[i] = int_sqrt(vReal[i]*vReal[i] + vImag[i]*vImag[i]);
 
-    // Adapted from open source Rainmeter's audio visualization code with
-    //  permission from the development team.
-    //  Relevant Github file: https://github.com/rainmeter/rainmeter/blob/master/Plugins/PluginAudioLevel/PluginAudioLevel.cpp
+    // Adapted from open source Rainmeter's audio visualization code with permission from the development team.
+    //  https://github.com/rainmeter/rainmeter/blob/master/Plugins/PluginAudioLevel/PluginAudioLevel.cpp
     int output[COLUMNS] = {0};
     int iBand = 0;
     int iBin = 0;
-    int binCount = 0;
-    int f0 = 0;
     while(iBand < COLUMNS && iBin <= SAMPLES/2){
-      binCount++;
       if(lin_fn[iBin] <= log_fn[iBand]){
-        if(vReal[iBin] > 16) output[iBand] += vReal[iBin];
+        if(vReal[iBin] > THRESHOLD) output[iBand] += vReal[iBin];
         iBin++;
       }
       else{
-        if(vReal[iBin] > 16) output[iBand] += vReal[iBin];
-        binCount = 0;
+        if(vReal[iBin] > THRESHOLD) output[iBand] += vReal[iBin];
         iBand++;
       }
     }
 
-    // Performs multiple operations: flattening, post-processing, and smoothing
-    uint16_t postprocess[64];
+    float postprocess[64];
     static float buff[64];
-    
     for(int iCol = 0; iCol < 64; iCol++){
-      // Combining imaginary and real data into a unified array
-      postprocess[iCol] = SENSITIVITY*(output[iCol]);
-      // Logarithmic scaling to create more visible output display height of 16
-      // log(0.5) term is added to compensate for buffer pulling at sampling stage
-      // log(SENSITIVITY) term is to emulate the above line (which MUST be used for
-      //  the below if statement for some reason) but with greater precision
-      //  TO-DO: Find out how to eleiminate dependence on above statement
-      if(postprocess[iCol] > 16)
-        postprocess[iCol] = 40.*(log((float)(output[iCol]))+log(0.0625)+log(SENSITIVITY));
+      // Applying log scale, log(0.0625) term compensates for 16x scaling before FFT and
+      //  log(SENSITIVITY) term applies sensitivity
+      if(output[iCol] > 16/SENSITIVITY)
+        postprocess[iCol] = 40*(log((float)(output[iCol]))+log(0.0625)+log(SENSITIVITY));
       else postprocess[iCol] = 0;   // Cuts off negative values before they are calculated
-      //
       
-      if(postprocess[iCol] > buff[iCol]){
-        // Smoothing by factoring in past data
-        postprocess[iCol] = buff[iCol] * anti_coeff + (float)postprocess[iCol] * coeff;
-        buff[iCol] = postprocess[iCol];       // Storing new output as next frame's past data
-      }
-      else{
-        postprocess[iCol] = buff[iCol] * anti_coeff2 + (float)postprocess[iCol] * coeff2;
-        buff[iCol] = postprocess[iCol];
-      }
+      // Exponential moving average smoothing
+      if(postprocess[iCol] > buff[iCol])
+        postprocess[iCol] = buff[iCol] * anti_coeff + postprocess[iCol] * coeff;
+      else
+        postprocess[iCol] = buff[iCol] * anti_coeff2 + postprocess[iCol] * coeff2;
+      buff[iCol] = postprocess[iCol];
     }
 
     // Cutting off the smaller numbers representing signals too weak to be relevant
@@ -238,16 +210,13 @@ void Task1code( void * pvParameters ){
     }
   
     // Translating output data into column heights, which is entered into the buffer
-    screenBuffer.swap_ready = false;  // REDUNDANT, Old code to block access to the buffer
-                                      //  TODO: Remove?
-    uint8_t *writeBuffer = (uint8_t*)screenBuffer.writeBuffer;
-    for(int i = 0; i < COLUMNS; i++) writeBuffer[i] = postprocess[i]*64/CAP;
-    screenBuffer.swap_ready = true;   // Signals to other core that write is complete, and
-                                      //  that it can switch the buffers
+    for(int i = 0; i < COLUMNS; i++) screenBuffer.writeBuffer[i] = 64*postprocess[i]/CAP;
+    screenBuffer.swap_ready = true;   // Raises flag to indicate buffer is ready to push
     
+    watchdogReset();
+
     // Outputs benchmark data
     frames++;
-    watchdogReset();
     static bool benchmark_posted = false;
     if(millis() > 5000 && !benchmark_posted){
       Serial.print(frames/5);
@@ -261,12 +230,7 @@ void Task1code( void * pvParameters ){
 void setup() {
     Serial.begin(115200);
     
-    pinMode(15, INPUT);
-
-    //*LED display output pins setup removed
-    
-    pinMode(39, INPUT);
-    pinMode(36, INPUT);
+    pinMode(INPUT_PIN, INPUT);
     pinMode(0, INPUT_PULLUP);
 
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -285,7 +249,6 @@ void setup() {
 }
 
 void loop() {
-    // Double-buffered output with page flipping to display
     if(screenBuffer.swap_ready){
       screenBuffer.swap();
       screenBuffer.swap_ready = false;
@@ -293,11 +256,11 @@ void loop() {
 
     display.clearDisplay();
     display.drawFastHLine(0, 63, 128, WHITE);
-    uint8_t *readBuffer = (uint8_t*)screenBuffer.readBuffer;
-    const int pixels_per_column = 128/COLUMNS;
+    const int col_px = 128/COLUMNS;
     for(int i = 0; i < COLUMNS; i++){
-      display.drawRect(i*pixels_per_column-COLUMN_SIZE, 64-readBuffer[i], COLUMN_SIZE, readBuffer[i], WHITE);
-      display.fillRect(i*pixels_per_column-COLUMN_SIZE, 64-readBuffer[i], COLUMN_SIZE, readBuffer[i], WHITE);
+      int length = screenBuffer.readBuffer[i];
+      display.drawRect(i*col_px-COLUMN_SIZE, 64-length, COLUMN_SIZE, length, WHITE);
+      display.fillRect(i*col_px-COLUMN_SIZE, 64-length, COLUMN_SIZE, length, WHITE);
       //display.drawFastVLine(i*pixels_per_column, 64-readBuffer[i], readBuffer[i], WHITE);
     }
     display.display();
@@ -315,10 +278,9 @@ void watchdogReset(){
 
 //* Flash display function definition removed
 
-// Recursively computes square root and replaces the Arduino-provided sqrt
-// function, which used floating point math. Outputs reasonably for any
-// number less than int32_t max but works fastest on smaller numbers like
-// in this case.
+// Recursively computes square root and replaces the Arduino-provided sqrt function, 
+// which used floating point math. Outputs reasonably for any number less than int32_t 
+// max but works fastest on smaller numbers.
 int16_t int_sqrt(int32_t val){
   // Initial values if n = 0
   int32_t Nsquared = 0;
